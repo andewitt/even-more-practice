983367a65b2cef3f96e4bf3c76f021a60e272a3e
Alexandra DEWITT <Sasha@Alexandras-MacBook-Air.local>
2016-09-25 12:42:35 -0400

    my name changes

--- even-more-practice.rb
+++ even-more-practice.rb
@@ -1,13 +1,13 @@
-#ask a user for their first name
-#store it in a variable called 'first_name'
-
-#ask a user for their middle name
-#store it in a variable called 'middle_name'
-
-#ask a user for their last name
-#store it in a variable called 'last_name'
+puts("What is your first name?")
+first_name = gets.strip.capitalize
 
 
-full_name = "#{first_name} + #{middle_name} + #{last_name}"
+puts("What is your middle name?")
+middle_name = gets.strip.capitalize
+
+puts("What is your last name?")
+last_name = gets.strip.capitalize
+
+full_name = "#{first_name} #{middle_name} #{last_name}"
 
 puts("Your full name is #{full_name}")
